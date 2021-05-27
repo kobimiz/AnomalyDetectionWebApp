4ad515c40c80b947615b96a771fd3693eae293c4
Kobi Mizrahi <kobimiz2001@gmail.com>
2021-05-28 00:53:24 +0300

    formatting

--- src/index.js
+++ src/index.js
@@ -37,79 +37,45 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 app.post('/uploadTrain', upload.single('data'), (req, res) => {
-    console.log('uploaded training file successfuly');
+    console.log('uploaded training file successfuly')
 })
 
 app.post('/uploadDetect', upload.single('data'), (req, res) => {
-    console.log('uploaded detection file successfuly');
+    console.log('uploaded detection file successfuly')
 })
 
 app.post('/detect', (req, res) => {
-    // let ts = new timeseries('./uploads/train.csv')
-    // let detObj;
-    // if (req.body.alg === 'Regression algorithm')
-    //     detObj = new SimpleAnomalyDetector()
-    // else if (req.body.alg === 'Hybrid algorithm')
-    //     detObj = new HybridAnomalyDetector();
-    // detObj.learnNormal(ts)
-    // res.send(req.file)
-
-    var socket = new net.Socket();
+    let socket = new net.Socket()
     socket.connect(8000, "localhost", function () {
-        console.log("Client: Connected to server");
-    });
-    
-    // Let's handle the data we get from the server
-    let result = '';
+        console.log("Client: Connected to server")
+    })
+
+    let result = ''
     socket.on("data", function (data) {
-        result += data.toString();
-        console.log(data.toString());
-    });
-    // socket.on('end', () => {
-    //     console.log(result);
-    //     res.send(result);
-    // })
-    let trainFile = fs.readFileSync('./uploads/train.csv').toString();
-    let detectFile = fs.readFileSync('./uploads/detect.csv').toString();
-    console.log('sending 1');
-    socket.write('1\n');
+        result += data.toString()
+    })
+    socket.on('end', () => {
+        res.send(result)
+    })
+
+    let trainFile = fs.readFileSync('./uploads/train.csv').toString()
+    let detectFile = fs.readFileSync('./uploads/detect.csv').toString()
+    socket.write('1\n')
     trainFile.split('\n').forEach(line => {
-        socket.write(line)
-        socket.write('\n')
-    });
-    socket.write('done\n');
+        socket.write(line + '\n')
+    })
+    socket.write('done\n')
     detectFile.split('\n').forEach(line => {
-        socket.write(line)
-        socket.write('\n')
-    });
-    socket.write('done\n');
+        socket.write(line + '\n')
+    })
+    socket.write('done\n')
 
-    if (req.body.alg == 'Regression algorithm') {
-        console.log('sending 7');
-        socket.write('7\n');
-    }
-    else {
-        console.log('sending 3');
-        socket.write('3\n');
-    }
-    console.log('sending 5');
-    socket.write('5\n');
-    socket.write(`60,70\n`);
-    socket.write(`70,74\n`);
-    socket.write(`done\n`);
-    socket.write('4\n');
-    socket.write(`6\n`);
+    if (req.body.alg == 'Regression algorithm')
+        socket.write('7\n')
+    else
+        socket.write('3\n')
 
-    res.send('3');
-
-    // let ts = new timeseries('./uploads/detect.csv')
-    // let detObj;
-    // if (req.body.alg === 'Regression algorithm')
-    //     detObj = new SimpleAnomalyDetector()
-    // else if (req.body.alg === 'Hybrid algorithm')
-    //     detObj = new HybridAnomalyDetector()
-    // const anomalies = detObj.detect(ts)
-
+    socket.write('5\n60,70\n70,74\ndone\n4\n6\n')
 })
 
 //POST Requests
