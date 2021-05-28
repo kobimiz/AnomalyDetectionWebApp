2bea789892a2e55d3a4a3fc2b53d1e3ccd0ce146
Kobi Mizrahi <kobimiz2001@gmail.com>
2021-05-28 15:31:56 +0300

    part 2 complete

--- src/index.js
+++ src/index.js
@@ -32,6 +32,41 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.text())
 
+// first two params are just strings containing the data
+// third takes the values: 'Regression algorithm' or 'Hybrid algorithm'
+// forth is the object used to send back to client
+// fifth is the function used to transform the data sent to the client
+async function trainDetect(trainFile, detectFile, type, res, transformFunc) {
+    if (type != 'Regression algorithm' && type != 'Hybrid algorithm')
+        return
+    let socket = new net.Socket()
+    socket.connect(8000, "localhost", () => {})
+
+    let result = ''
+    socket.on("data", function (data) {
+        result += data.toString()
+    })
+    socket.on('end', () => {
+        res.send(transformFunc(result))
+    })
+    socket.write('1\n')
+    trainFile.split('\n').forEach(line => {
+        socket.write(line + '\n')
+    })
+    socket.write('done\n')
+    detectFile.split('\n').forEach(line => {
+        socket.write(line + '\n')
+    })
+    socket.write('done\n')
+
+    if (type == 'Regression algorithm')
+        socket.write('7\n')
+    else (type == 'Regression algorithm')
+        socket.write('3\n')
+
+    socket.write('4\n6\n')
+}
+
 //GET Requestss
 app.get('/', (req, res) => {
     res.render('index')
@@ -45,38 +80,38 @@ app.post('/uploadDetect', upload.single('data'), (req, res) => {
 })
 
 app.post('/detect', (req, res) => {
-    let socket = new net.Socket()
-    socket.connect(8000, "localhost", () => {})
-
-    let result = ''
-    socket.on("data", function (data) {
-        result += data.toString()
-    })
-    socket.on('end', () => {
-        res.send(result)
-    })
-
-    let trainFile = fs.readFileSync('./uploads/train.csv').toString()
-    let detectFile = fs.readFileSync('./uploads/detect.csv').toString()
-    socket.write('1\n')
-    trainFile.split('\n').forEach(line => {
-        socket.write(line + '\n')
-    })
-    socket.write('done\n')
-    detectFile.split('\n').forEach(line => {
-        socket.write(line + '\n')
-    })
-    socket.write('done\n')
-
-    if (req.body.alg == 'Regression algorithm')
-        socket.write('7\n')
-    else
-        socket.write('3\n')
-
-    socket.write('5\n60,70\n70,74\ndone\n4\n6\n')
+    let body = JSON.parse(req.body)
+    trainDetect(fs.readFileSync('./uploads/train.csv').toString(), fs.readFileSync('./uploads/detect.csv').toString(), body.alg, res, res => res)
 })
 
-//POST Requests
+
+app.post('/', (req, res) => {
+    /**
+     * request body format:
+     * {
+     *  alg: regression/hybrid,
+     *  trainData: ...,
+     *  detectData: ...
+     * }
+     */
+    if (req.body.alg == undefined || req.body.trainData == undefined || req.body.detectData == undefined) {
+        res.json({error: "Not a valid request format"})
+    } else {
+        trainDetect(req.body.trainData, req.body.detectData, req.body.alg, res, result => {
+            let arr = result.replace(/[ \t]+/g, ' ').split('\n')
+            arr.pop()
+            return JSON.stringify(arr.map(row => {
+                let splitted = row.split(' ')
+                let obj = {}
+                obj[splitted[0]] = splitted[1]
+                return obj
+            }))
+        })
+    }
+
+
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
